1d3fb7950b92d5f057e80322274bda50309ea943
elin-d <wapamtw9778>
2020-11-12 12:30:48 +0300

    Add a new argument output_type

--- compute.py
+++ compute.py
@@ -1,4 +1,4 @@
-def compute_kaya_identity(pop, gdp, enInt, carbInt):
+def compute_kaya_identity(pop, gdp, enInt, carbInt, output_type="CO2"):
     """
     Equation that expresses yearly CO2 emissions.
 
@@ -10,10 +10,12 @@ def compute_kaya_identity(pop, gdp, enInt, carbInt):
     enInt(numeric): Energy Intensity (in Gigajoule/$1000GDP). Energy Intensity is the energy needed to produce a certain
     amount of economic value.
     carbInt(numeric) Carbon Intensity (in tonnes CO2/Gigajoule). Carbon Intensity is the CO2 emitted for produced energy
-
+    output_type (String): "CO2" or "C", by default "CO2". If it was set to "C", the results will be given in
+    tonnes C instead of tonnes CO2.
 
     Returns:
-    float: CO2 emissions in million tonnes of CO2
+    float: CO2 or C emissions in million tonnes respectively
+    :param output_type:
 
     """
     if pop < 0:
@@ -22,8 +24,12 @@ def compute_kaya_identity(pop, gdp, enInt, carbInt):
         raise ValueError("The GDP per capita should be positive.")
 
     co2 = pop * gdp * enInt * carbInt
-    return co2
-
+    if output_type == "C":
+        return co2/3.67
+    elif output_type == "CO2":
+        return co2
+    else:
+        ValueError("Output type should be CO2 or C")
 
 def main():
     result = compute_kaya_identity(1, 1, 1, 1)
--- kaya_identity_testing.py
+++ kaya_identity_testing.py
@@ -5,25 +5,28 @@ from compute import compute_kaya_identity
 
 class TestKID(unittest.TestCase):
 
-    def test_calc(self):
-        self.assertEqual(compute_kaya_identity(1, 1, 1, 1), 1, "Should be 1")
+    def test_calc_co2(self):
+        self.assertEqual(compute_kaya_identity(1, 1, 1, 1, "CO2"), 1, "Should be 1")
+
+    def test_calc_c(self):
+        self.assertAlmostEqual(compute_kaya_identity(3.67, 1, 1, 1, "C"), 1, msg="Should be 1", places=5)
 
     def test_negative_intensity(self):
-        compute_kaya_identity(1, 1, 1, -1)
-        compute_kaya_identity(1, 1, -1, 1)
+        compute_kaya_identity(1, 1, 1, -1, "CO2")
+        compute_kaya_identity(1, 1, -1, 1, "CO2")
         pass
 
     def test_negative_population(self):
         with self.assertRaises(ValueError):
-            compute_kaya_identity(-1, 1, 1, 1)
+            compute_kaya_identity(-1, 1, 1, 1, "CO2")
 
     def test_negative_gdp(self):
         with self.assertRaises(ValueError):
-            compute_kaya_identity(1, -1, 1, 1)
+            compute_kaya_identity(1, -1, 1, 1, "CO2")
 
     def test_string_as_input(self):
         with self.assertRaises(TypeError):
-            compute_kaya_identity("1", "1", "1", "1")
+            compute_kaya_identity("1", "1", "1", "1", "CO2")
 
 
 if __name__ == '__main__':
